1b4c0db3fc294a65661c324f592cd8d4c24adb19
ahqsa24 <adhiet353@gmail.com>
2025-12-05 16:54:29 +0700

    fix: animation on statsgrid

--- src/components/organisms/StatsGrid.tsx
+++ src/components/organisms/StatsGrid.tsx
@@ -13,6 +13,16 @@ export const StatsGrid = () => (
         <StatBox number={25} label="Instruktur Bersertifikasi" />
         <StatBox number={"100%"} label="Lulusan Terserap" />
       </div>
+
+      {/* duplicate content for infinite loop */}
+      <div className="flex gap-16 px-64">
+        <StatBox number={10} label="Unit Kompetisi" />
+        <StatBox number={"98%"} label="Positive Review" />
+        <StatBox number={"200+"} label="Peserta" />
+        <StatBox number={2} label="Program Pelatihan" />
+        <StatBox number={25} label="Instruktur Bersertifikasi" />
+        <StatBox number={"100%"} label="Lulusan Terserap" />
+      </div>
     </div>
   </section>
 );
